c0bdedccc6d32df90ea1a99d736fdba9f83b1b89
Ryutooooo <rf.ryutooooo.0907@gmail.com>
2021-02-01 01:23:32 +0900

    fix: go fmt

--- ast/ast_test.go
+++ ast/ast_test.go
@@ -9,22 +9,22 @@ import (
 )
 
 func TestString(t *testing.T) {
-  program := &Program{
-    Statements: []Statement{
-      &LetStatement{
-        Token: token.Token{Type: token.LET, Literal: "let"},
-        Name: &Identifier{
-          Token: token.Token{Type: token.IDENT, Literal: "myVar"},
-          Value: "myVar",
-        },
-        Value: &Identifier{
-          Token: token.Token{Type:token.IDENT, Literal: "anotherVar"},
-          Value: "anotherVar",
-        },
-      },
-    },
-  }
-  if program.String() != "let myVarr = anotherVar;" {
-    t.Errorf("program.String(0 wrong. got=%q", program.String())
-  }
+	program := &Program{
+		Statements: []Statement{
+			&LetStatement{
+				Token: token.Token{Type: token.LET, Literal: "let"},
+				Name: &Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "myVar"},
+					Value: "myVar",
+				},
+				Value: &Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "anotherVar"},
+					Value: "anotherVar",
+				},
+			},
+		},
+	}
+	if program.String() != "let myVarr = anotherVar;" {
+		t.Errorf("program.String(0 wrong. got=%q", program.String())
+	}
 }
