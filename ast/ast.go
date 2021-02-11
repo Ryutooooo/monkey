9cf94c0399850217fa970f81386be388e2c447f4
Ryutooooo <rf.ryutooooo.0907@gmail.com>
2021-02-11 22:06:23 +0900

    feat: define function literal

--- ast/ast.go
+++ ast/ast.go
@@ -4,6 +4,7 @@ package ast
 
 import (
 	"bytes"
+	"strings"
 
 	"github.com/Ryutooooo/monkey/token"
 )
@@ -124,6 +125,31 @@ type IntegerLiteral struct {
 func (il *IntegerLiteral) expressionNode()      {}
 func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
 func (il *IntegerLiteral) String() string       { return il.Token.Literal }
+
+type FunctionLiteral struct {
+	Token      token.Token // 'fn'
+	Parameters []*Identifier
+	Body       *BlockStatement
+}
+
+func (fl *FunctionLiteral) expressionNode()      {}
+func (fl *FunctionLiteral) TokenLiteral() string { return fl.Token.Literal }
+func (fl *FunctionLiteral) String() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range fl.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString(fl.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") ")
+	out.WriteString(fl.Body.String())
+
+	return out.String()
+}
 
 type PrefixExpression struct {
 	Token    token.Token // e.g. ! etc...
